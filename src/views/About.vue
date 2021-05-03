fea2323957d06e4735b376400abc331131abf3dd
Lucas Ng <lucsngyileang@gmail.com>
2021-05-03 13:52:52 +0800

    added calendar function

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="calendar">
+    <h1>Calendar</h1>
+    <Calendar />
   </div>
 </template>
+
+<script>
+import Calendar from "../components/Calendar.vue";
+
+export default {
+  components: { Calendar },
+  setup() {},
+};
+</script>
