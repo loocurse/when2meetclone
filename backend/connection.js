95648df52e52de5b6a65a479ffe28bdb2cf8974a
Lucas Ng <lucasngyileang@gmail.com>
2021-05-25 17:25:45 +0800

    Remove logging

--- backend/connection.js
+++ backend/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const { username, password } = require("./credentials.json");
 
-const URI = `mongodb+srv://${username}:${password}when2meetclone@cluster0.0lnwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+const URI = `mongodb+srv://${username}:${password}@cluster0.0lnwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const connectDB = async () => {
   await mongoose.connect(URI, {
     useNewUrlParser: true,
