8299a508bcf66f24c1331eb000274c9789a9f68c
Lucas Ng <lucasngyileang@gmail.com>
2021-05-25 16:59:45 +0800

    Add password to env file

--- backend/connection.js
+++ backend/connection.js
@@ -4,15 +4,12 @@ const mongoose = require("mongoose");
 const URI = `mongodb+srv://kehvinbehvin:when2meetclone@cluster0.0lnwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://lucasng:NEq3B960kb19neWe@cluster0.0lnwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  );
+  await mongoose.connect(process.env.MONGO_PROD_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
   console.log("Database has been connected");
 };
 
