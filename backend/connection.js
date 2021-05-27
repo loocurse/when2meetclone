11f2f462ec7bcac1fe218aa73a938b24ba01c95d
Lucas Ng <lucasngyileang@gmail.com>
2021-05-27 17:09:42 +0800

    Use process environment variables

--- backend/connection.js
+++ backend/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-if (process.env.NODE_ENV === 'production') {
-  const username = process.env.DB_USERNAME
-  const password = process.env.DB_PASSWORD
+if (process.env.NODE_ENV === "production") {
+  const URI = process.env.MONGO_PROD_URI;
+  console.log(process.env)
 } else {
   const { username, password } = require("./credentials.json");
+  const URI = `mongodb+srv://${username}:${password}@cluster0.0lnwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 }
 
-const URI = `mongodb+srv://${username}:${password}@cluster0.0lnwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const connectDB = async () => {
   await mongoose.connect(URI, {
     useNewUrlParser: true,
