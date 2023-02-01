1b5d9d746bdc7384e257a2232a3f480f709c5883
Lucas Ng <lucasngyileang@gmail.com>
2023-02-01 15:12:38 +0800

    fix: remove read from credentials.json

--- backend/connection.js
+++ backend/connection.js
@@ -1,14 +1,7 @@
 const mongoose = require("mongoose");
-if (process.env.NODE_ENV === "production") {
-  const URI = process.env.MONGO_PROD_URI;
-  console.log(process.env)
-} else {
-  const { username, password } = require("./credentials.json");
-  const URI = `mongodb+srv://${username}:${password}@cluster0.0lnwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-}
 
 const connectDB = async () => {
-  await mongoose.connect(URI, {
+  await mongoose.connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
